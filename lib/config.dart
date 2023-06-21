ce0eec3c8edf7e163b774857ee96c5cbff7c4c55
Abhinav-gupta103 <ag0537269@gmail.com>
2023-06-22 00:17:18 +0530

    frontend code push

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+final url = 'http://192.168.1.3:3000/';
+final registration = url + "registration";
+final login = url + 'login';
+final addtodo = url + 'create-todo';
+final getToDoList = url + 'getUserTodos';
+final deleteTodo = url + 'deleteTodo';
